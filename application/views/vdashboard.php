79c2076a4f422304955b5e4d985dfba9e408dde5
JackCloudman <maylo360xd@gmail.com>
2018-10-07 02:56:51 -0500

    add dashboard view

--- application/views/vdashboard.php
+++ application/views/vdashboard.php
@@ -3,8 +3,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Page Header
-        <small>Optional description</small>
+        Dashboard<small>(panel de control)</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -14,11 +13,233 @@
 
     <!-- Main content -->
     <section class="content container-fluid">
+      <div class="row">
+        
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>4</h3>
 
-      <!--------------------------
-        | Your Page Content Here |
-        -------------------------->
+              <p>Eventos registrados</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-calendar"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más informacion<i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>350</h3>
 
+              <p>Likes</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-heart"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más informacion<i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-teal">
+            <div class="inner">
+              <h3>54</h3>
+
+              <p>Comentarios nuevos</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-comments"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver comentarios<i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Lista de eventos activos</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+             <div class="row">
+          <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-black" style="background: url('assets/dist/img/photo1.png') center center;">
+              <h3 class="widget-user-username">Entrega reconocimientos</h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map-marker"></i> Centro Cultural Jaime Torres Bodet</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">300</h5>
+                    <span class="description-text"><i class="fa fa-paper-plane"></i> Invitaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">124</h5>
+                    <span class="description-text"><i class="fa fa-check"></i> Confirmaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+          <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-black" style="background: url('assets/dist/img/photo2.png') center center;">
+              <h3 class="widget-user-username">Toma de protesta</h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map-marker"></i> Auditorio ESCOM</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">115</h5>
+                    <span class="description-text"><i class="fa fa-paper-plane"></i> Invitaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">84</h5>
+                    <span class="description-text"><i class="fa fa-check"></i> Confirmaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+          <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-black" style="background: url('assets/dist/img/photo3.jpg') center center;">
+              <h3 class="widget-user-username">Vista a la BUAP</h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map-marker"></i> BUAP</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">420</h5>
+                    <span class="description-text"><i class="fa fa-paper-plane"></i> Invitaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">312</h5>
+                    <span class="description-text"><i class="fa fa-check"></i> Confirmaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+          <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-black" style="background: url('assets/dist/img/photo4.jpg') center center;">
+              <h3 class="widget-user-username">Conferencia Microsoft</h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map-marker"></i> ESCOM - Sala 14</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">120</h5>
+                    <span class="description-text"><i class="fa fa-paper-plane"></i> Invitaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">57</h5>
+                    <span class="description-text"><i class="fa fa-check"></i> Confirmaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+          <div class="col-md-4">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-black" style="background: url('https://i.imgur.com/QQsuibl.jpg?fb') center center;">
+              <h3 class="widget-user-username">Cumpleaños Haruhi!!</h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map-marker"></i> Brigada SOS</h5>
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">1000</h5>
+                    <span class="description-text"><i class="fa fa-paper-plane"></i> Invitaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-6 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">999</h5>
+                    <span class="description-text"><i class="fa fa-check"></i> Confirmaciones</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+                <!-- /.col -->
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div>
+          </div>
+          <!-- /.widget-user -->
+        </div>
+
+      </div>
+            </div>
+            <!-- ./box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        
+      </div>
     </section>
     <!-- /.content -->
 </div>
