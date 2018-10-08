5c0edde32c92377e47e5a34a5acbecf944bf71f6
OscarArmas <saga_cth@hotmail.com>
2018-10-07 19:41:18 -0500

    prueba

---
+++ application/controllers/NuevoEvento.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class NuevoEvento extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('headers/vheader',array("title"=>"hola mundo"));
+		$this->load->view('vdashboard');
+		$this->load->view('footers/vfooter');
+	}
+}
