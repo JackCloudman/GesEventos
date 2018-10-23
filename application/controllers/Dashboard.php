360edacb950616beb074d9912c2654807bdaad4c
Jack Cloudman <maylo360xd@gmail.com>
2018-10-23 11:22:56 -0500

    Validacion login Nuevo evento

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,18 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-	function __construct(){
-  		parent::__construct();
-  		$this->load->library('session');
- 		$user = $this->session->userdata("user");
- 		if(!$user){
-			redirect('login');
-		}
-  	}
-	public function index()
-	{
-		$this->load->view('headers/vheader',array("title"=>"hola mundo"));
-		$this->load->view('vdashboard');
-		$this->load->view('footers/vfooter');
-	}
+    function __construct(){
+        parent::__construct();
+        $this->load->library('session');
+        $user = $this->session->userdata("user");
+        if(!$user){
+            redirect('login');
+        }
+    }
+    public function index()
+    {
+        $this->load->view('headers/vheader',array("title"=>"hola mundo"));
+        $this->load->view('vdashboard');
+        $this->load->view('footers/vfooter');
+    }
 }
