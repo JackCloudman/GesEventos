ef3d028311e1977fdbd79996d9ac321f2991e6b1
Jack Cloudman <maylo360xd@gmail.com>
2018-11-11 14:46:49 -0600

    Arreglo de headers

--- application/views/headers/vheaderDoc.php
+++ application/views/headers/vheaderDoc.php
@@ -13,22 +13,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>GesEventos | <?=$title?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="/GesEventos-master/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/GesEventos-master/assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="/GesEventos-master/assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/GesEventos-master/assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect. -->
-  <link rel="stylesheet" href="/GesEventos-master/assets/dist/css/skins/skin-red.min.css">
-<script src="/GesEventos-master/assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <link rel="stylesheet" href="<?=base_url()?>assets/dist/css/skins/skin-red.min.css">
+<script src="<?=base_url()?>assets/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/GesEventos-master/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/GesEventos-master/assets/dist/js/adminlte.min.js"></script>
+<script src="<?=base_url()?>assets/dist/js/adminlte.min.js"></script>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -88,14 +88,14 @@ desired effect
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!-- The user image in the navbar-->
-              <img src="/GesEventos-master/assets/dist/img/escom.png" class="user-image" alt="User Image">
+              <img src="<?=base_url()?>assets/dist/img/escom.png" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
               <span class="hidden-xs">ESCOM - IPN</span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
               <li class="user-header">
-                <img src="/GesEventos-master/assets/dist/img/escom.png" class="img-circle" alt="User Image">
+                <img src="<?=base_url()?>assets/dist/img/escom.png" class="img-circle" alt="User Image">
 
                 <p>
                   ESCOM - IPN
