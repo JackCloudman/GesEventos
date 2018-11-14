bff4eb4aba8adf5e2dd715d034422d2621b96ecf
jackcloudman <maylo360xd@gmail.com>
2018-11-14 09:53:13 -0600

    Fix design and conection to db

--- application/views/vcalendario.php
+++ application/views/vcalendario.php
@@ -1,72 +1,64 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jonat
- * Date: 11/13/2018
- * Time: 9:08 PM
- */?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8' />
-    <link href="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.css" rel="stylesheet"/>
-    <link href="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.print.min.css" rel="stylesheet" media="print"/>
-    <script src="<?=base_url()?>/assets/bower_components/moment/min/moment.min.js"></script>
-    <script src="<?=base_url()?>/assets/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
-    <script>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard<small> (Super Admin)</small>
+      </h1>
+    </section>
 
-        $(document).ready(function() {
-            $.post('<?php echo base_url();?>Calendar/getEventos',
-            function (data) {
-                alert(data);
-                $('#calendar').fullCalendar({
-                    header: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'month,basicWeek,basicDay'
-                    },
-                    defaultDate: new Date(),
-                    navLinks: true, // can click day/week names to navigate views
-                    editable: true,
-                    eventLimit: true, // allow "more" link when too many events
-                    events: $.parseJSON(data)
-                });
-            }
-            );
-
-
-
-        });
-
-    </script>
-    <style>
-
-        body {
-            margin: 40px 10px;
-            padding: 0;
-            font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-            font-size: 14px;
-        }
-
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Lista de usuarios</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div id='calendar'></div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+<link href="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.css" rel="stylesheet"/>
+<link href="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.print.min.css" rel="stylesheet" media="print"/>
+<script src="<?=base_url()?>/assets/bower_components/moment/min/moment.min.js"></script>
+<script src="<?=base_url()?>/assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?=base_url()?>/assets/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $.post('<?php echo base_url();?>Calendar/getEventos',
+    function (data) {
+      //alert(data);
+      $('#calendar').fullCalendar({
+          header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay'
+          },
+          defaultDate: new Date(),
+          navLinks: true, // can click day/week names to navigate views
+          editable: true,
+          eventLimit: true, // allow "more" link when too many events
+          events: $.parseJSON(data)
+      });
+    }
+    );
+  });
+</script>
+<style>
         #calendar {
             max-width: 900px;
             margin: 0 auto;
         }
 
     </style>
-</head>
-<body>
-<div class="content-wrapper">
-    <div class="box-header with-border">
-        <h3 class="box-title" style="float: center">Publicar nuevo Evento.</h3>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        <div id='calendar'></div>
-    </div>
-</div>
-</body>
-</html>
-
-
