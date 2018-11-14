8ef84daccc4e640e037ed4d5d5537f1d44b13d52
jackcloudman <maylo360xd@gmail.com>
2018-11-14 09:30:53 -0600

    Modify conection to db

--- application/models/Mevento.php
+++ application/models/Mevento.php
@@ -10,16 +10,16 @@ class Mevento extends CI_Model
                       "ponente"=>$ponente,
                       "descripcion"=>$descripcion,
                       "fecha"=>$fecha,
-                      "escuela"=>$escuela,
+                      "escuela"=>1,
                       "auditorio"=>$Auditorio,
                       "hora_inicio" => $hora_inicio);
-        $result = $this->db->insert('eventos', $data);
+        $result = $this->db->insert('Eventos', $data);
         return $result;
 	}
 	public function infoPorEvento()
 	{
 		$this->db->select('nombre_evento, fecha, escuela, hora_inicio');
-		$this->db->from('eventos');
+		$this->db->from('Eventos');
 		return $this->db->get()->result(); 
 
 	}
