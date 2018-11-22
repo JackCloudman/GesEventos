d87a5220f322c06f2d4fa9017c99cac973895199
OscarArmas <oarmass1300@alumno.ipn.mx>
2018-11-21 20:31:56 -0600

    evento view listo

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,23 +4,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
     function __construct(){
         parent::__construct();
-        $this->load->library('session');
-        $usuario = $this->session->userdata("user");
-        if(!$usuario){
+        $this->usuario = $this->session->userdata("user");
+        if(!$this->usuario){
             redirect('login');
         }
-        if($usuario->nivel==3)
+        if($this->usuario->nivel==3)
             redirect('Superadmin');
-        if($usuario->nivel==2)
+        if($this->usuario->nivel==2)
             redirect('Admin');
         $this->load->model('mevento');
+        $this->load->model('musuario');
     }
     public function index()
     {
-
-
+        $usuario = $this->usuario;
+        $datosh['head'] =$this->musuario->getName($usuario->id_usuario);
         $datos['datos'] = $this->mevento->infoPorEvento();
-        $this->load->view('headers/vheaderuser',array("title"=>"Dashboard general"));
+        $this->load->view('headers/vheaderuser',$datosh);
         $this->load->view('Guest/vdashboard',$datos);
         $this->load->view('footers/vfooter');
     }
