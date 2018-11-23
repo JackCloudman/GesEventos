b990eb024e1cda4bc506f72a3ce73d0c23f848da
jackcloudman <maylo360xd@gmail.com>
2018-11-23 13:41:01 -0600

    test to generate boleto

--- application/controllers/Boleto.php
+++ application/controllers/Boleto.php
@@ -5,20 +5,28 @@ class Boleto extends CI_Controller
     {
         parent::__construct();
         require_once(APPPATH . '../vendor/autoload.php');
+        $this->load->model('mboleto');
     }
     public function index(){
-      $mpdf = new \Mpdf\Mpdf([
-      	'margin_left' => 20,
-      	'margin_right' => 15,
-      	'margin_top' => 25,
-      	'margin_bottom' => 25,
-      	'margin_header' => 10,
-      	'margin_footer' => 10,
-      	'showBarcodeNumbers' => FALSE
-      ]);
-      $html=$this->load->view('vindex',[],true);
-      $mpdf->WriteHTML("<p>hola mundo</p>");
-      $mpdf->Output(); // opens in browser
-
+      return;
+    }
+    public function imprimir($id_boleto=null){
+      if(!$id_boleto)
+        return;
+        $mpdf = new \Mpdf\Mpdf([
+        	'margin_left' => 20,
+        	'margin_right' => 15,
+        	'margin_top' => 25,
+        	'margin_bottom' => 25,
+        	'margin_header' => 10,
+        	'margin_footer' => 10,
+        	'showBarcodeNumbers' => FALSE
+        ]);
+        $boleto = $this->mboleto->get($id_boleto);
+        if(!$boleto)
+          return;
+        $html=$this->load->view('Guest/vboleto',Array("boleto"=>$boleto),true);
+         $mpdf->WriteHTML($html);
+        $mpdf->Output(); // opens in browser
     }
 }
